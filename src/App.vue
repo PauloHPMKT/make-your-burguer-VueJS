d2bb5b0bd839be1d25bea6a3b0671d375d7f554d
Paulo Sergio <pauloserg861@gmail.com>
2022-05-18 18:28:26 -0300

    organizing home structure

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ export default {
 
   .main-container {
     margin: 50px;
-    min-height: 560px;
+    min-height: 250px;
   }
 
   h1 {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>Página Home</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>formulário</p>
   </div>
+
 </template>
+
+<script>
+import Banner from '../components/Banner.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+  }
+}
+</script>
