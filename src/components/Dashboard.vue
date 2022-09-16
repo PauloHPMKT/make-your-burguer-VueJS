8c076e0066ca1f58d3d696a3427c42d2d30a5bcc
Paulo Sergio <pauloserg861@gmail.com>
2022-09-16 20:51:36 -0300

    criando pagina e componente de pedidos / dashboard

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="burger-table">
+    <div>
+      <div id="burger-table-header">
+         <div class="order-id">#:</div> 
+         <div>Cliente:</div>
+         <div>Pão:</div>
+         <div>Carne:</div>
+         <div>Opcionais:</div>
+         <div>Acoes:</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+        <div class="order-number">{{ burger.id }}</div>
+        <div>{{ burger.name }}</div>
+        <div>{{ burger.pao }}</div>
+        <div>{{ burger.carne }}</div>
+        <div>
+          <ul>
+            <li v-for="(opcional, index) in burger.opcionais" :key="index">{{ opcional }}</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">{{  }}</option>
+          </select>
+          <button class="delete-btn">Cancelar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      burgers: null,
+      burger_id: null,
+      status: [],
+    }
+  },
+  methods: {
+    async getOrdes() {
+
+      const req = await fetch('http://localhost:3000/burgers')
+      const data  = await req.json()
+
+      this.burgers = data
+
+      //resgatar os status
+
+
+    }
+  },
+  mounted() {
+    this.getOrdes()
+  } 
+}
+</script>
+
+<style scoped>
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burger-table-header, 
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-header {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+#burger-table-header div,
+.burger-table-row div {
+  width: 19%;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #ccc;
+}
+
+#burger-table-header .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+  width: 50%;
+}
+
+.delete-btn {
+  background-color: #222;
+  color: #fcba03;
+  font-weight: bold;
+  border: 2px solid #222;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: .5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+
+</style>
--- src/views/PedidosView.vue
+++ src/views/PedidosView.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="main-container">
-    <h1>Página de pedidos</h1>
+    <h1>Gerenciar pedidos:</h1>
+    <dashboard />
   </div>
 </template>
+
+<script>
+import Dashboard from '../components/Dashboard.vue'
+
+export default {
+  name: 'Pedidos',  
+  components: { Dashboard },
+}
+</script>
 
 <style>
 
